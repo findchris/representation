d39c2eab70ca8a500f7226e68810588f36cdc73a
Chris Johnson <findchris@gmail.com>
2011-07-13 11:50:57 -0700

    Leverage ActiveSupport::Concern for main module.

---
+++ lib/representation.rb
@@ -0,0 +1,17 @@
+require 'active_support'
+
+module Representation
+  extend ActiveSupport::Concern
+
+  module ClassMethods
+    def representation
+      
+    end
+  end
+
+  module InstanceMethods
+    def representation
+      
+    end
+  end
+end
--- spec/representation_spec.rb
+++ spec/representation_spec.rb
@@ -1,7 +1,24 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "Representation" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  context "the module" do
+    it "should be includable" do
+      class Dummy
+        include Representation
+      end
+    end
+    it "should make available the representation class method" do
+      class Dummy
+        include Representation        
+        representation
+      end
+    end
+    it "should make available the representation instance method" do
+      class Dummy
+        include Representation        
+        representation
+      end
+      Dummy.new.representation
+    end
   end
 end
