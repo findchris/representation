fe0e1561057c13ba834c84bb20751187d0b96740
Chris Johnson <findchris@gmail.com>
2011-07-13 12:37:41 -0700

    Commit progress towards object cloning.

--- lib/representation.rb
+++ lib/representation.rb
@@ -2,16 +2,22 @@ require 'active_support'
 
 module Representation
   extend ActiveSupport::Concern
+  
+  included do
+    class_attribute :representations
+    self.representations = {}
+  end
 
   module ClassMethods
-    def representation
-      
+    def representation(name, *attributes_and_method_names)
+      representations[name] = attributes_and_method_names
     end
   end
 
   module InstanceMethods
-    def representation
-      
+    def representation(name)
+      represented_object = clone
+      represented_object
     end
   end
 end
--- spec/representation_spec.rb
+++ spec/representation_spec.rb
@@ -10,15 +10,72 @@ describe "Representation" do
     it "should make available the representation class method" do
       class Dummy
         include Representation        
-        representation
+        representation :ignored, :ignored
       end
     end
     it "should make available the representation instance method" do
       class Dummy
         include Representation        
-        representation
+        representation :ignored, :ignored
       end
-      Dummy.new.representation
+      Dummy.new.representation(:ignored)
+    end
+  end
+  context ".representation" do
+    it "should accept the name of the representation as the first argument" do
+      class Dummy
+        include Representation        
+        representation :public
+      end
+    end
+    it "should accept a list of attributes and method names that make up the named representation" do
+      class Dummy
+        include Representation        
+        representation :public, :name, :calculated_age
+        
+        attr_accessor :name, :age
+        
+        def calculated_age
+          age * 2
+        end
+      end
+    end
+  end
+  context "#representation" do
+    it "should return the same type of object as the receiver" do
+      class Dummy
+        include Representation        
+        representation :public, :name, :calculated_age
+        
+        attr_accessor :name, :age
+        
+        def calculated_age
+          age * 2
+        end
+      end
+      
+      dummy = Dummy.new
+      dummy.representation(:public).should be_an_instance_of(Dummy)
+    end
+    it "should return an object with only the attributes identified by the representation definition" do
+      class Dummy
+        include Representation        
+        representation :public, :name, :calculated_age
+
+        attr_accessor :name, :age, :ssn
+
+        def calculated_age
+          age * 2
+        end
+      end
+
+      dummy = Dummy.new
+      dummy.name = 'Tweedle Dum'
+      dummy.age  = 42
+      public_dummy = dummy.representation(:public)
+      public_dummy.name.should == 'Tweedle Dum'
+      public_dummy.calculated_age.should == 84
+      lambda { public_dummy.ssn }.should raise_error(NoMethodError)
     end
   end
 end
