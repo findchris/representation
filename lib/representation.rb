3bb396aac3d3e78709dbba587a56119247ae482c
Chris Johnson <findchris@gmail.com>
2011-07-13 18:29:32 -0700

    Define attr_accessors on the representation's singleton class.

--- lib/representation.rb
+++ lib/representation.rb
@@ -22,15 +22,21 @@ module Representation
 
   module InstanceMethods
     def representation(name)
-      represented_attributes = self.class.values_for_representation(name).inject({}) do |attributes, val|
+      attributes_and_method_names = self.class.values_for_representation(name)
+      represented_attributes = attributes_and_method_names.inject({}) do |attributes, val|
         attributes.merge({val.to_s => send(val)})
       end
-      
+
       clone.tap do |represented_object|
         represented_object.instance_variable_set('@attributes', represented_attributes)
         
+        represented_attributes.each do |key, value|
+          represented_object.singleton_class.send :attr_accessor, key.to_sym
+          represented_object.send "#{key}=".to_sym, value
+        end
+        
         def represented_object.attributes
-          represented_attributes
+          @attributes
         end
         
         def represented_object.inspect
--- spec/representation_spec.rb
+++ spec/representation_spec.rb
@@ -2,60 +2,60 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'ruby-debug'
 
 describe "Representation" do
-  # context "the module" do
-  #   it "should be includable" do
-  #     class Dummy
-  #       include Representation
-  #     end
-  #   end
-  #   it "should make available the representation class method" do
-  #     class Dummy
-  #       include Representation        
-  #       representation :ignored, :ignored
-  #     end
-  #   end
-  #   it "should make available the representation instance method" do
-  #     class Dummy
-  #       include Representation        
-  #       representation :ignored, :ignored
-  #     end
-  #     Dummy.new.representation(:ignored)
-  #   end
-  # end
-  # context ".representation" do
-  #   it "should accept the name of the representation as the first argument" do
-  #     class Dummy
-  #       include Representation        
-  #       representation :public, :ignored
-  #     end
-  #     Dummy.representation_names.should include(:public)
-  #   end
-  #   it "should accept a list of attributes and method names that make up the named representation" do
-  #     class Dummy
-  #       include Representation        
-  #       representation :public, :name, :calculated_age
-  #       
-  #       attr_accessor :name, :age
-  #       
-  #       def calculated_age
-  #         age * 2
-  #       end
-  #     end
-  #     Dummy.values_for_representation(:public).should == [:name, :calculated_age]
-  #   end
-  # end
+  context "the module" do
+    it "should be includable" do
+      class Dummy
+        include Representation
+      end
+    end
+    it "should make available the representation class method" do
+      class Dummy
+        include Representation        
+        representation :ignored, :ignored
+        attr_accessor :ignored
+      end
+    end
+    it "should make available the representation instance method" do
+      class Dummy
+        include Representation        
+        representation :ignored, :ignored
+        attr_accessor :ignored
+      end
+      Dummy.new.representation(:ignored)
+    end
+  end
+  context ".representation" do
+    it "should accept the name of the representation as the first argument" do
+      class Dummy
+        include Representation        
+        representation :public, :ignored
+      end
+      Dummy.representation_names.should include(:public)
+    end
+    it "should accept a list of attributes and method names that make up the named representation" do
+      class Dummy
+        include Representation        
+        representation :public, :name, :calculated_age
+        
+        attr_accessor :name, :age
+        
+        def calculated_age
+          age * 2
+        end
+      end
+      Dummy.values_for_representation(:public).should == [:name, :calculated_age]
+    end
+  end
   context "#representation" do
-    # it "should return the same type of object as the receiver" do
-    #   user = User.new
-    #   user.representation(:public).should be_an_instance_of(User)
-    # end
+    it "should return the same type of object as the receiver" do
+      user = User.new(:age => 42)
+      user.representation(:public).should be_an_instance_of(User)
+    end
     it "should return an object with only the attributes identified by the representation definition" do
       user = User.new
       user.name = 'Tweedle Dum'
       user.age  = 42
       public_user = user.representation(:public)
-      
-      debugger
       
       public_user.name.should == 'Tweedle Dum'
       public_user.calculated_age.should == 84
